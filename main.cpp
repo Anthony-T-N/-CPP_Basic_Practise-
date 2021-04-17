4177646cfa315656d424fbbd87577fe128a5ab63
Anthony-T-N <53560209+Anthony-T-N@users.noreply.github.com>
2021-04-17 17:41:16 +0930

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,39 @@
+// -CPP_Basic_Practise-.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+// Used for Input/output.
+#include <iostream>
+
+// Renamed cpp file in "Source Files" from -CPP_Basic_Practise-.cpp to main.cpp
+int main()
+{
+    // Write to console.
+    std::cout << "Hello World!\n";
+    std::cout << "1\n";
+    std::cout << "2\n";
+    std::cout << "3\n";
+    std::cout << "\n";
+    
+    int aint = 123;
+    std::cout << aint;
+    std::string astring = "Test";
+    std::cout << astring;
+    std::cout << "\n";
+
+    for (int i = 0; i <= 10; i++)
+    {
+        std::cout << i << "\n";
+    }
+    return 0;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
